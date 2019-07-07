c958470a9fe15c1fbae72fea26442a09b244338b
mcarmonadev <permacultura@gmail.com>
2019-07-07 17:16:50 -0400

    Fix caracter faltante
    mising ')'

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ console.log('Babel & ReactJs Setup ');
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = ('Babel & ReactJs ya configurado';
+const title = ('Babel & ReactJs ya configurado');
 
 ReactDOM.render(
   <div>{title}</div>,
